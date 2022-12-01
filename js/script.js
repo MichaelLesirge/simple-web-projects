57bf90e3fb5f39312a9380bb98b8df6f8c5d7b3e
Michael Lesirge <michaellesirge@gmail.com>
2022-11-30 21:09:18 -0800

    allow user to move side boxed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
-document.querySelectorAll(".info-container-toggle").forEach(el => {
-	const labelFor = document.getElementById(el.getAttribute("for"))
+let z = 1
+
+document.querySelectorAll(".info-container-toggle").forEach(label => {
+	const infoConainer = document.getElementById(label.getAttribute("for"))
 
 	toggle = () => {
-		el.innerText = el.innerText === "<" ? ">" : "<"
-		labelFor.classList.toggle("hidden")
+		label.innerText = label.innerText === "<" ? ">" : "<"
+		infoConainer.classList.toggle("hidden")
 	}
 
 	if (window.innerWidth < 800) toggle()
 
-	el.onclick = toggle;
+	label.onclick = toggle;
+
+	let isDown = false;
+	let offset = 0
+
+	infoConainer.addEventListener("mousedown", (event) => {
+		infoConainer.style.opacity = 0.7
+		infoConainer.style.zIndex = z
+
+		z++;
+
+		isDown = true;
+		offset = infoConainer.offsetTop - event.clientY
+	})
+
+	infoConainer.addEventListener("mouseup", (event) => {
+		isDown = false;
+
+		infoConainer.style.zIndex = 1
+		infoConainer.style.opacity = 1
+	})
+
+	document.addEventListener("mousemove", (event) => {
+		if (isDown) infoConainer.style.top  = (event.clientY + offset) + 'px'
+	})
 
 })
