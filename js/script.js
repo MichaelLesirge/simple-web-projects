f3e4666252ac0a1329d2914b4f539d2ba3cccbed
Michael Lesirge <michaellesirge@gmail.com>
2022-12-03 19:21:14 -0800

    make sure button interactions don't move sidebars

--- js/script.js
+++ js/script.js
@@ -16,21 +16,40 @@ document.querySelectorAll(".info-container-toggle").forEach((label) => {
 
 	// add dragging
 	let isDown = false;
+	let isInputing = false;
 	let offset = 0;
 
 	const startListener = ["mousedown", "touchstart"];
 	const endListeners = ["mouseup", "touchend", "mouseleave"];
 
+	infoContainer.querySelectorAll("input, select, option").forEach((element) => {
+		startListener.forEach((eventType) => {
+			element.addEventListener(eventType, (event) => {
+				isInputing = true
+			})
+		})
+	})
+
+	infoContainer.querySelectorAll("input, select, option").forEach((element) => {
+		endListeners.forEach((eventType) => {
+			element.addEventListener(eventType, (event) => {
+				isInputing = false
+			})
+		})
+	})
+
 	startListener.forEach((eventType) => {
 		infoContainer.addEventListener(eventType, (event) => {
-			container.style.opacity = 0.7;
-			label.style.opacity = 1;
-
-			container.style.zIndex = z;
-			z++;
-
-			isDown = true;
-			offset = container.offsetTop - event.clientY;
+			if (!isInputing) {
+				container.style.opacity = 0.7;
+				label.style.opacity = 1;
+	
+				container.style.zIndex = z;
+				z++;
+	
+				isDown = true;
+				offset = container.offsetTop - event.clientY;
+			}
 		});
 	});
 
