fc3516497fcc3eaa733629021f34efa0c32f3c0a
Michael Lesirge <michaellesirge@gmail.com>
2022-12-03 20:25:54 -0800

    improve grabbing for side bars

--- js/script.js
+++ js/script.js
@@ -20,49 +20,36 @@ document.querySelectorAll(".info-container-toggle").forEach((label, index) => {
 	// add dragging
 	(() => {
 		let isDown = false;
-		let isInputing = false;
 		let offset = 0;
 
+		const statSectionTitle = infoContainer.querySelector(".stat-section-title")
+
 		const startListener = ["mousedown", "touchstart"];
-		const endListeners = ["mouseup", "touchend", "mouseleave"];
+		const endListeners = ["mouseup", "touchend"];
 
-		infoContainer.querySelectorAll("input, select, option").forEach((element) => {
-			startListener.forEach((eventType) => {
-				element.addEventListener(eventType, (event) => {
-					isInputing = true;
-				});
-			});
-		});
-
-		infoContainer.querySelectorAll("input, select, option").forEach((element) => {
-			endListeners.forEach((eventType) => {
-				element.addEventListener(eventType, (event) => {
-					isInputing = false;
-				});
-			});
-		});
+		statSectionTitle.style.cursor = "grab";
 
 		startListener.forEach((eventType) => {
-			infoContainer.addEventListener(eventType, (event) => {
-				if (!isInputing) {
-					container.style.opacity = 0.7;
-					label.style.opacity = 1;
+			statSectionTitle.addEventListener(eventType, (event) => {
+				container.style.opacity = 0.7;
+				statSectionTitle.style.cursor = "grabbing";
+				label.style.opacity = 1;
 
-					if (index !== lastMoved) {
-						zIndexeMax++;
-						lastMoved = index;
-						container.style.zIndex = zIndexeMax;
-					}
-
-					isDown = true;
-					offset = container.offsetTop - event.clientY;
+				if (index !== lastMoved) {
+					zIndexeMax++;
+					lastMoved = index;
+					container.style.zIndex = zIndexeMax;
 				}
+
+				isDown = true;
+				offset = container.offsetTop - event.clientY;
 			});
 		});
 
 		endListeners.forEach((eventType) => {
-			infoContainer.addEventListener(eventType, (event) => {
+			statSectionTitle.addEventListener(eventType, (event) => {
 				isDown = false;
+				statSectionTitle.style.cursor = "grab";
 
 				container.style.opacity = 1;
 			});
