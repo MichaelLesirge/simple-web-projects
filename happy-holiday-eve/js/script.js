d98447deab3016f34dc04b679da2dec505ef3794
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-01-11 17:02:27 -0800

    start on holiday selector

---
+++ happy-holiday-eve/js/script.js
@@ -0,0 +1,23 @@
+const today = new Date();
+holdays = [
+    {
+        name: "new-years",
+        humanReadableName: "new-years",
+        startDate: new Date(today.getFullYear(), 0, 1)
+    },
+    {
+        name: "christmas",
+        humanReadableName: "christmas",
+        startDate: new Date(today.getFullYear(), 0, 1)
+    },
+    {
+        name: "hanukkah",
+        humanReadableName: "Hanukkah",
+        startDate: new Date(today.getFullYear(), 0, 1)
+    },
+    {
+        name: "Kwanzaa",
+        humanReadableName: "kwanzaa",
+        startDate: new Date(today.getFullYear(), 0, 1)
+    },
+]
