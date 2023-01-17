17165c6985a2f2801ad9c0ba5dfff54434a146c4
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-01-16 20:39:20 -0800

    formatting

--- happy-holiday-eve/js/script.js
+++ happy-holiday-eve/js/script.js
@@ -1,72 +1,67 @@
-"use strict"
+"use strict";
 
 const today = new Date();
-const oneDay = 24 * 60 * 60 * 1000; 
+const oneDay = 24 * 60 * 60 * 1000;
 
 const holidays = {
-    ny: {
-        name: "New Years",
-        merryMessage: "Happy",
-        startDate: new Date(today.getFullYear()+1, 0, 1)
-    },
-    xmas: {
-        name: "Christmas",
-        merryMessage: "Merry",
-        startDate: new Date(today.getFullYear(), 11, 25)
-    },
-    // Waiting for hebrew calendar implantation in javascript
-    // han: {
-    //     name: "Hanukkah",
-    //     merryMessage: "Happy",
-    //     startDate: new Date(today.getFullYear(), 11, 18)
-    // },
-    kz: {
-        name: "Kwanzaa",
-        merryMessage: "Happy",
-        startDate: new Date(today.getFullYear(), 11, 26)
-    },
-    // // for debuging
-    // today: {
-    //     name: "today",
-    //     startDate: today
-    // }
-}
+	ny: {
+		name: "New Years",
+		merryMessage: "Happy",
+		startDate: new Date(today.getFullYear() + 1, 0, 1),
+	},
+	xmas: {
+		name: "Christmas",
+		merryMessage: "Merry",
+		startDate: new Date(today.getFullYear(), 11, 25),
+	},
+	// Waiting for hebrew calendar implantation in javascript
+	// han: {
+	//     name: "Hanukkah",
+	//     merryMessage: "Happy",
+	//     startDate: new Date(today.getFullYear(), 11, 18)
+	// },
+	kz: {
+		name: "Kwanzaa",
+		merryMessage: "Happy",
+		startDate: new Date(today.getFullYear(), 11, 26),
+	},
+};
 
 const holidaySpan = document.querySelector("#holiday-name");
 const eveMessageSpan = document.querySelector("#eve-message");
 const merryMessageSpan = document.querySelector("#merry-message");
 
 function setHoliday(holiday) {
-    holidaySpan.innerText = holiday.name;
-    merryMessageSpan.innerText = holiday.merryMessage
+	holidaySpan.innerText = holiday.name;
+	merryMessageSpan.innerText = holiday.merryMessage;
 
-    const daysTillHoliday = getDaysTill(holiday.startDate);
+	const daysTillHoliday = getDaysTill(holiday.startDate);
 
-    eveMessageSpan.innerText = "Eve ".repeat(daysTillHoliday)
-    eveMessageSpan.title = `Eve × ${daysTillHoliday}`
+	eveMessageSpan.innerText = "Eve ".repeat(daysTillHoliday);
+	eveMessageSpan.title = `Eve × ${daysTillHoliday}`;
 
-    console.log(daysTillHoliday)
+	console.log(daysTillHoliday);
 }
 
 function getDaysTill(date) {
-    return Math.round((date - today) / oneDay)
+	return Math.round((date - today) / oneDay);
 }
 
-setHoliday(holidays.ny)
+setHoliday(holidays.ny);
 
-const selectHolidayInput = document.querySelector("#holidays-select")
+const selectHolidayInput = document.querySelector("#holidays-select");
 
 // add each holiday to menu
 for (const [name, holiday] of Object.entries(holidays)) {
-    const option = document.createElement("option")
-    option.value = name;
-    option.innerText = holiday.name
-    selectHolidayInput.add(option)
-};
+	const option = document.createElement("option");
+	option.value = name;
+	option.innerText = holiday.name;
+	selectHolidayInput.add(option);
+}
 
 selectHolidayInput.onchange = (event) => {
-    setHoliday(holidays[event.target.value])
-}
+	setHoliday(holidays[event.target.value]);
+};
 
 // TODO add differnt condions if message overflows
 // window.scrollTo({ right: 0, behavior: 'auto' });
