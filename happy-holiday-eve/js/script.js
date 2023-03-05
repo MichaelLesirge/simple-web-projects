1b767b70f59a0863d418d27d47d8b1123d5e9f30
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-03-05 14:44:23 -0800

    fix day of errors

--- happy-holiday-eve/js/script.js
+++ happy-holiday-eve/js/script.js
@@ -1,35 +1,49 @@
 "use strict";
 
-let today = new Date();
+let now = new Date();
 const oneDay = 24 * 60 * 60 * 1000;
+
+const selectHolidayInput = document.querySelector("#holidays-select");
+const messageElement = document.querySelector("#message");
+const countDownClockElement = document.querySelector("#count-down-clock");
 
 const holidays = {
 	ny: {
-		name: "New Years",
-		merryMessage: "Happy",
-		startDate: new Date(today.getFullYear() + (today.getMonth() != 0 || today.getDay() != 1), 0, 1),
+		message: "Happy New Years",
+		startDate: new Date(now.getFullYear(), 0, 1),
 	},
 	xmas: {
-		name: "Christmas",
-		merryMessage: "Merry",
-		startDate: new Date(today.getFullYear(), 11, 25),
+		message: "Merry Christmas",
+		startDate: new Date(now.getFullYear(), 11, 25),
 	},
-	// Waiting for hebrew calendar implantation in javascript
-	// TODO maybe add end date and say "Happy first/second/third... day of "
 	han: {
-		name: "Hanukkah",
-		merryMessage: "Happy",
-		startDate: today,
+		message: "Happy Hanukkah",
+		startDate: now,
 	},
 	kz: {
-		name: "Kwanzaa",
-		merryMessage: "Happy",
-		startDate: new Date(today.getFullYear(), 11, 26),
+		message: "Happy Kwanzaa",
+		startDate: new Date(now.getFullYear(), 11, 26),
 	},
 };
 
+// add each holiday to menu
+for (const [name, holiday] of Object.entries(holidays)) {
+	const option = document.createElement("option");
+	option.value = name;
+	option.innerText = holiday.message;
+	selectHolidayInput.add(option);
+}
+
+function isInPast(date) {
+	return date < now;
+}
+
+function isToday(date) {
+	return date.setHours(0, 0, 0, 0) == new Date().setHours(0, 0, 0, 0);
+}
+
 function getTimeTo(date) {
-	let delta = Math.abs(date - today) / 1000;
+	let delta = Math.abs(date - now) / 1000;
 
 	const days = Math.floor(delta / 86400);
 	delta -= days * 86400;
@@ -45,43 +59,39 @@ function getTimeTo(date) {
 	return [days, hours, minutes, seconds];
 }
 
-const holidayElement = document.querySelector("#holiday-name");
-const eveMessageElement = document.querySelector("#eve-message");
-const merryMessageElement = document.querySelector("#merry-message");
-const selectHolidayInput = document.querySelector("#holidays-select");
-const countDownClockElement = document.querySelector("#count-down-clock");
-
-let holiday = holidays.ny;
-
-// add each holiday to menu
-for (const [name, holiday] of Object.entries(holidays)) {
-	const option = document.createElement("option");
-	option.value = name;
-	option.innerText = holiday.name;
-	selectHolidayInput.add(option);
-}
+let holidayId = localStorage.getItem("current") ?? Object.entries(holidays)[0][0];
+selectHolidayInput.value = holidayId;
 
 selectHolidayInput.onchange = (event) => {
-
 	if (event.target.value == "han") {
-		alert("Unknown start date because I don't know how Hebrew calendar works or how to code that. Sorry!");
-		return
+		setTimeout(() => {
+			alert("Unknown start date because I don't know how Hebrew calendar works or how to code that. Sorry!");
+			selectHolidayInput.value = holidayId;
+		}, 200);
+		return;
 	}
 
-	holiday = holidays[event.target.value];
-
-	holidayElement.innerText = holiday.name;
-	merryMessageElement.innerText = holiday.merryMessage;
-
+	holidayId = event.target.value;
+	localStorage.setItem("current", holidayId);
 };
 
 setInterval(() => {
-	today = new Date()
+	now = new Date();
 
-	const [days, hours, minutes, seconds] = getTimeTo(holiday.startDate);
+	const holiday = holidays[holidayId];
 
-	eveMessageElement.innerText = "Eve ".repeat(days);
-	eveMessageElement.title = `Eve × ${days}`;
+	let [days, hours, minutes, seconds] = getTimeTo(holiday.startDate);
+
+	if (isToday(holiday.startDate)) {
+		[days, hours, minutes, seconds] = [0, 0, 0, 0];
+	} else if (isInPast(holiday.startDate)) {
+		holiday.startDate.setFullYear(holiday.startDate.getFullYear() + 1);
+	}
+
+	const daysRoundedUp = days + Boolean(hours || minutes || seconds);
+	
+	message.innerText = `${holiday.message} ${"Eve ".repeat(daysRoundedUp)}`;
+	message.title = `${holiday.message} Eve × ${daysRoundedUp}`;
 
 	countDownClockElement.innerText = [
 		makeClockFormat(days),
@@ -89,23 +99,16 @@ setInterval(() => {
 		makeClockFormat(minutes),
 		makeClockFormat(seconds),
 	].join(":");
+
+	countDownClockElement.title = `${makeClockTitleFormat(days, "day")}, ${makeClockTitleFormat(hours, "hour")}, ${makeClockTitleFormat(minutes, "minute")}, and ${makeClockTitleFormat(seconds, "second")} to ${holiday.startDate.toString().split("00:00:00")[0]}`
 	
-	countDownClockElement.title = [	
-		makeClockTitleFormat(days, "day"),
-		makeClockTitleFormat(hours, "hour"),
-		makeClockTitleFormat(minutes, "minute"),
-		makeClockTitleFormat(seconds, "second"),
-	].join(", ");
 });
 
-function makeClockFormat(time_unit_amount, padding=2) {
+
+function makeClockFormat(time_unit_amount, padding = 2) {
 	return String(time_unit_amount).padStart(padding, "0");
 }
 
 function makeClockTitleFormat(time_unit_amount, time_name) {
-	return `${time_unit_amount} ${time_name}${time_unit_amount >= 2 ? "s" : ""}`
+	return `${time_unit_amount} ${time_name}${time_unit_amount === 1 ? "" : "s"}`;
 }
-
-// TODO add differnt condions if message overflows
-// window.scrollTo({ right: 0, behavior: 'auto' });
-// window.scrollTo({ left: 0, behavior: 'smooth' });
