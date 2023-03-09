c056d40929023eb47fe5bc0826522df1fe8a0d0a
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-03-08 22:58:56 -0800

    add way to make custom dates

--- happy-holiday-eve/js/script.js
+++ happy-holiday-eve/js/script.js
@@ -26,67 +26,73 @@ const holidays = {
 	},
 };
 
+let customCount = 0;
+
+const customs = JSON.parse(localStorage.getItem("customs")) ?? [];
+for (const [message, date] of customs) {
+	addCustomOption(message, parceDate(date), holidays);
+}
+
 // add each holiday to menu
-for (const [name, holiday] of Object.entries(holidays)) {
+for (const [key, holiday] of Object.entries(holidays)) {
+	addOption(key, holiday.message);
+}
+
+addOption(
+	"custom",
+	centerText(" custom ", Object.values(holidays).reduce((sum, val) => Math.max(sum, val.message.length), 0),"-")
+);
+
+function addOption(key, message) {
 	const option = document.createElement("option");
-	option.value = name;
-	option.innerText = holiday.message;
+	option.value = key;
+	option.innerText = message;
 	selectHolidayInput.add(option);
 }
 
-function isInPast(date) {
-	return date < now;
-}
-
-function isToday(date) {
-	return new Date(date).setHours(0, 0, 0, 0) == new Date().setHours(0, 0, 0, 0);
-}
-
-function getTimeTo(date) {
-	let delta = Math.abs(date - now) / 1000;
-
-	const days = Math.floor(delta / 86400);
-	delta -= days * 86400;
-
-	const hours = Math.floor(delta / 3600) % 24;
-	delta -= hours * 3600;
-
-	const minutes = Math.floor(delta / 60) % 60;
-	delta -= minutes * 60;
-
-	const seconds = Math.floor(delta);
-
-	return [days, hours, minutes, seconds];
-}
-
-let holidayId = localStorage.getItem("current") ?? Object.entries(holidays)[0][0];
-selectHolidayInput.value = holidayId;
+let holidayKey = localStorage.getItem("current") ?? Object.entries(holidays)[0][0];
+selectHolidayInput.value = holidayKey;
 
 selectHolidayInput.onchange = (event) => {
 	if (event.target.value == "han") {
 		setTimeout(() => {
 			alert("Unknown start date because I don't know how Hebrew calendar works or how to code that. Sorry!");
-			selectHolidayInput.value = holidayId;
+			selectHolidayInput.value = holidayKey;
 		}, 200);
 		return;
+	} else if (event.target.value == "custom") {
+		let message = prompt("Enter message: ");
+		let date = parceDate(prompt("Enter date (mm/dd): "));
+		
+		while (date === null) {
+			date = parceDate(prompt("Invalid date. Enter correct date (mm/dd): "));
+		}
+
+		const key = addCustomOption(message, date, holidays);
+		saveCustomOption(message, date)
+
+		addOption(key, holidays[key].message)
+
+		selectHolidayInput.value = key;
+
 	}
 
-	holidayId = event.target.value;
-	localStorage.setItem("current", holidayId);
+	holidayKey = event.target.value;
+	localStorage.setItem("current", holidayKey);
 };
 
 let last;
 setInterval(() => {
 	now = new Date();
 
-	const holiday = holidays[holidayId];
+	const holiday = holidays[holidayKey];
 
-	if (isInPast(holiday.startDate)) {
+	while (isInPast(holiday.startDate, now)) {
 		holiday.startDate.setFullYear(holiday.startDate.getFullYear() + 1);
 	}
 
-	const [days, hours, minutes, seconds] = isToday(holiday.startDate) ? [0, 0, 0, 0] : getTimeTo(holiday.startDate);
-	
+	const [days, hours, minutes, seconds] = isToday(holiday.startDate, now) ? [0, 0, 0, 0] : getTimeTo(holiday.startDate, now);
+
 	const daysRoundedUp = days + Boolean(hours || minutes || seconds);
 
 	if (last !== daysRoundedUp) {
@@ -112,4 +118,59 @@ function makeClockFormat(time_unit_amount, padding = 2) {
 
 function makeClockTitleFormat(time_unit_amount, time_name) {
 	return `${time_unit_amount} ${time_name}${time_unit_amount === 1 ? "" : "s"}`;
+}
+
+function addCustomOption(message, date, obj) {
+	const key = `custom${customCount}`;
+	obj[key] = {
+		message: message,
+		startDate: date,
+	};
+	customCount++;
+	return key;
+}
+
+function saveCustomOption(message, date) {
+	localStorage.setItem(
+		"customs",
+		JSON.stringify([...JSON.parse(localStorage.getItem("customs")) ?? [], [message, `${date.getMonth() + 1}/${date.getDay()}`]])
+	);
+}
+
+function parceDate(dateText) {
+	const split = dateText.split("/");
+	const date = new Date(now.getFullYear(), split[0] - 1, split[1]);
+	if (date.getDate() === NaN) return;
+	return date;
+}
+
+function isInPast(date, now) {
+	return date < now;
+}
+
+function isToday(date, now) {
+	return new Date(date).setHours(0, 0, 0, 0) == new Date(now).setHours(0, 0, 0, 0);
+}
+
+function getTimeTo(date, now) {
+	let delta = Math.abs(date - now) / 1000;
+
+	const days = Math.floor(delta / 86400);
+	delta -= days * 86400;
+
+	const hours = Math.floor(delta / 3600) % 24;
+	delta -= hours * 3600;
+
+	const minutes = Math.floor(delta / 60) % 60;
+	delta -= minutes * 60;
+
+	const seconds = Math.floor(delta);
+
+	return [days, hours, minutes, seconds];
+}
+
+function centerText(text, finalLen, char = " ") {
+	finalLen -= text.length;
+	finalLen = Math.ceil(finalLen / 2);
+	return char.repeat(finalLen) + text + char.repeat(finalLen);
 }
