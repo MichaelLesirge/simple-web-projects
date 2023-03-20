2d05aa6cbe335a752d6b3718f056308107d2903d
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-03-19 17:51:54 -0700

    add counter

--- quickWebScripts/focusV2.js
+++ quickWebScripts/focusV2.js
@@ -8,20 +8,29 @@ const messages = [
 	"Stay on task",
 ];
 
-const messageUtterances = messages.map((message) => new SpeechSynthesisUtterance(message));
-
 const timeout = 60 * 2 * 1000;
 
 let lastMoveTime = new Date();
+
+let count = 0;
+
+const special = ['zeroth','first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth'];
+const deca = ['twent', 'thirt', 'fort', 'fift', 'sixt', 'sevent', 'eight', 'ninet'];
+
+function stringifyNumber(n) {
+	if (n < 20) return special[n];
+	if (n % 10 === 0) return deca[Math.floor(n / 10) - 2] + "ieth";
+	return deca[Math.floor(n / 10) - 2] + "y-" + special[n % 10];
+}
 
 function randomChoice(array) {
 	return array[Math.floor(Math.random() * array.length)];
 }
 
 function sayRandomMessage() {
-	const randMessageUtterance = randomChoice(messageUtterances);
-	window.speechSynthesis.speak(randMessageUtterance);
-	console.log(randMessageUtterance.text);
+	count++;
+	const randMessage = randomChoice(messages);
+	window.speechSynthesis.speak(new SpeechSynthesisUtterance(`For the ${stringifyNumber(count)} time, ` + randMessage));
 }
 
 function isPageHidden() {
@@ -35,7 +44,7 @@ window.addEventListener("visibilitychange", (event) => {
 let intervalId;
 ["mousemove", "keydown", "click"].forEach((lictenerType) => {
 	window.addEventListener(lictenerType, (event) => {
-        clearInterval(intervalId);
+		clearInterval(intervalId);
 		intervalId = setInterval(sayRandomMessage, timeout);
 	});
 });
