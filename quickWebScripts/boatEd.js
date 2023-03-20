0abf515712c83c5ba388f77d47942819be1d11ea
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-03-19 22:12:18 -0700

    de-squish script and add log to show weeks left

--- quickWebScripts/boatEd.js
+++ quickWebScripts/boatEd.js
@@ -1,10 +1,13 @@
-f = (s) => {
-	console.log(s);
-	("setTimeout(()=>alert(s),10)");
-};
-x = Array(...document.querySelectorAll(".page"));
-c = x.filter((e) => e.classList.contains("complete")).length;
-f(`${c} of ${x.length} complete, ${x.length - c} to go. ${((c / x.length) * 10).toFixed(2)}% done.`);
-d = Number(document.querySelector('a[href="/washington/#taking-the-course"]').innerText.split(" ")[1]);
-f(`Must complete ${((x.length - c) / d).toFixed(2)} pages every day to complete on time`);
-`${c}/${x.length}`;
+const allPages = Array(...document.querySelectorAll(".page"));
+
+const daysLeft = Number(document.querySelector('a[href="/washington/#taking-the-course"]').innerText.split(" ")[1]);
+
+const totalPages = allPages.length
+const completedPages = allPages.filter((e) => e.classList.contains("complete")).length;
+const pagesLeft = totalPages - completedPages
+
+console.log(`${completedPages} of ${totalPages} complete, ${pagesLeft} to go. ${((completedPages / totalPages) * 10).toFixed(2)}% done.`);
+console.log(`Currently have ${daysLeft} days (${Math.floor(daysLeft / 7)}${daysLeft % 7 === 0 ? "" : "+"} weeks) to complete this cource.`)
+console.log(`Must complete ${(pagesLeft / daysLeft).toFixed(2)} pages every day to complete on time`);
+
+`${completedPages}/${totalPages}`;
