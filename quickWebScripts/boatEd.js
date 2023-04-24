3972b10b56d963428ea376d752647beb4e41f19e
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-04-23 19:07:56 -0700

    fix typo

--- quickWebScripts/boatEd.js
+++ quickWebScripts/boatEd.js
@@ -6,7 +6,7 @@ const totalPages = allPages.length
 const completedPages = allPages.filter((e) => e.classList.contains("complete")).length;
 const pagesLeft = totalPages - completedPages
 
-console.log(`${completedPages} of ${totalPages} complete, ${pagesLeft} to go. ${((completedPages / totalPages) * 10).toFixed(2)}% done.`);
+console.log(`${completedPages} of ${totalPages} complete, ${pagesLeft} to go. ${((completedPages / totalPages) * 100).toFixed(2)}% done.`);
 console.log(`Currently have ${daysLeft} days (${Math.floor(daysLeft / 7)}${daysLeft % 7 === 0 ? "" : "+"} weeks) to complete this cource.`)
 console.log(`Must complete ${(pagesLeft / daysLeft).toFixed(2)} pages every day to complete on time`);
 
