52e5fe01ac7438689065c822b08f7cd9fd7b0331
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-06-05 00:50:56 -0700

    add cursed text option

--- text-changer/script.js
+++ text-changer/script.js
@@ -1,13 +1,21 @@
 "use strict";
 
-String.prototype.toCapitalized = function toCapitalized() {
+String.prototype.toCapitalized = function () {
 	return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 };
+
+function randChar(startCharCode, stopCharCode) {
+    return String.fromCharCode(Math.floor(Math.random() * (stopCharCode - startCharCode)) + startCharCode)
+}
+ 
+function curseText(text, amount) {
+	return Array.from(text, (char) => char + (/[a-zA-Z0-9]/.test(char) ? Array.from({length: amount}, () => randChar(768, 879)).join("") : "")).join("")
+}
 
 const converters = {
 	"Case": {
 		"Upper Case": (text) => text.toUpperCase(),
-		"Lower Case": (text) => text.toLocaleLowerCase(),
+		"Lower Case": (text) => text.toLowerCase(),
 		"Title Case": (text) => text.split(" ").map((word) => word.toCapitalized()).join(" "),
 		"Random Case": (text) => text.split("").map((char) => Math.random() > 0.5 ? char.toUpperCase() : char.toLowerCase()).join(""),
 	},
@@ -18,35 +26,28 @@ const converters = {
 		"Pascal Case": (text) => text.split(" ").map((word) => word.toCapitalized()).join(""),
 	},
 	"Binary Encoding": {
-		"ASCII": (text) => Array.from(text).map((char) => char.charCodeAt(0).toString(2).padStart(8, "0")).join(" "),
+		"ASCII": (text) => Array.from(text, (char) => char.charCodeAt(0).toString(2).padStart(8, "0")).join(" "),
 		"Unicode UTF-8": (text) => Array.from(new TextEncoder().encode(text)).map((byte) => byte.toString(2)).join(" "),
 	},
 	"Unicode": {
-		"Decimal": (text) => Array.from(text).map((char) => char.charCodeAt(0).toString(10)).join(" "),
-		"Code": (text) => Array.from(text).map((char) => "U+" + char.charCodeAt(0).toString(16).toUpperCase().padStart(4, "0")).join(" ")
+		"Decimal": (text) => Array.from(text, (char) => char.charCodeAt(0).toString(10)).join(" "),
+		"Code": (text) => Array.from(text, (char) => "U+" + char.charCodeAt(0).toString(16).toUpperCase().padStart(4, "0")).join(" ")
 	},
 	"Cursed": {
-		"Low": (text) => text,
-		"Meduim": (text) => text,
-		"High": (text) => text,
+		"Low": (text) => curseText(text, 5),
+		"Meduim": (text) => curseText(text, 25),
+		"High": (text) => curseText(text, 50),
 	},
 	"Direction": {
 		"Reverse": (text) => text.split("").reverse().join(""),
 	},
-	// "Meme": {
-	// 	"Leetspeak": (text) => {
-	// 		leet_converter = {
-	// 			"a": "@",
-	// 			"b": "ß",
-	// 			"c": "©",
-	// 			"e": "3",
-	// 			"o": "0",
-	// 			"p": "9",
-
-	// 		}
-	// 		return text
-	// 	},
-	// },
+	"Meme": {
+		"Leetspeak": (text) => {
+			const leet_converter = {"e": "3", "t": "7", "i": "1", "o": "0", "a": "4", "s": "5", "g": "9", "l": "1", "z": "2", "b": "8"}
+			return text.split("").map((char) => (char.toLowerCase() in leet_converter && Math.random()) > 0.001 ? leet_converter[char] : char).join("")
+		},
+		"Cow": (text) => text.split(" ").map((word) => Array.from(word, (char, i) => char === char.toLowerCase() ? (i ? "o" : "m") : (i ? "O" : "M")).join("")).join(" "),
+	},
 };
 
 const inputBox = document.querySelector("#input");
@@ -82,22 +83,22 @@ for (const [sectionName, sectionItems] of Object.entries(converters)) {
         const inputElement = document.createElement("input");
         inputElement.id = sectionName +  " " + itemName;
 		inputElement.name = sectionName;
-        inputElement.type = "radio"
-        labelElement.appendChild(inputElement)
+        inputElement.type = "radio";
+        labelElement.appendChild(inputElement);
 		
 		if (itemName === "Default") {
-			inputElement.setAttribute("checked", true)
+			inputElement.setAttribute("checked", true);
 		}
 
         inputElement.addEventListener("input", (event) => {
 			usedConversions = usedConversions.filter((e) => Object.values(sectionItems).indexOf(e) === -1);
 			usedConversions.push(converter);
-			console.log(usedConversions)
+			console.log(usedConversions);
 		})
 
-		inputElement.addEventListener("change", updateBox)
+		inputElement.addEventListener("change", updateBox);
 
-        sectionElement.appendChild(labelElement)
+        sectionElement.appendChild(labelElement);
     }
 
     controlGroupsContainer.appendChild(sectionElement);
