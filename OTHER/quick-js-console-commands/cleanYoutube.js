f577c213b980201739a1210ca5acedfb9bd38964
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-06-27 20:03:23 -0700

    hide scroll bar

--- OTHER/quick-js-console-commands/cleanYoutube.js
+++ OTHER/quick-js-console-commands/cleanYoutube.js
@@ -1,1 +1,1 @@
-[document.querySelector("div#container.style-scope.ytd-masthead"), document.querySelector("div#below.style-scope.ytd-watch-flexy")].forEach(e => e.remove())
+["div#container.style-scope.ytd-masthead", "div#below.style-scope.ytd-watch-flexy"].forEach(e => document.querySelector(e).remove());setInterval(() => document.body.style.overflow = "hidden", 1000)
