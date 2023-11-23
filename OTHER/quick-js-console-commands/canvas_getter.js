e6ce8627ea4307923a9a9769c2cdd2eb15ada729
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-11-22 16:33:57 -0800

    make script reusable

--- OTHER/quick-js-console-commands/canvas_getter.js
+++ OTHER/quick-js-console-commands/canvas_getter.js
@@ -1,3 +1,3 @@
-const dataURL = canvas.toDataURL("image/png");
-const newTab = window.open('about:blank','image from canvas');
+let dataURL = $0.toDataURL("image/png");
+let newTab = window.open('about:blank','image from canvas');
 newTab.document.write("<img src='" + dataURL + "' alt='from canvas'/>");
