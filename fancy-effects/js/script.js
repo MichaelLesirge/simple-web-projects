a5ec36e35dfe8f9ef8ba3d6805d7b66dacc0f791
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2024-06-25 16:09:44 -0700

    fix tetris animation

--- fancy-effects/js/script.js
+++ fancy-effects/js/script.js
@@ -1,3 +1,5 @@
+// Not the best code here, just wanted it to work
+
 {
     const dragDuration = 200;
 
@@ -89,7 +91,7 @@ setInterval(() => {
 }, 100)
 
 const FPS = 60;
-function startLoop(element, init, clear, nextFrame, { resetOnScroll = false, resetOnClick = false, alwaysRun = false} = {}) {
+function startLoop(element, init, clear, nextFrame, { resetOnScroll = false, resetOnClick = false, alwaysRun = false, resetOnResize = true} = {}) {
     init();
     nextFrame()
 
@@ -114,7 +116,6 @@ function startLoop(element, init, clear, nextFrame, { resetOnScroll = false, res
         requestAnimationFrame(loop);
     }
 
-
     loop();
 
     function reset() {
@@ -124,6 +125,7 @@ function startLoop(element, init, clear, nextFrame, { resetOnScroll = false, res
 
     if (resetOnClick) element.addEventListener("click", reset)
     if (resetOnScroll) respondToVisibility(element, reset)
+    if (resetOnResize) window.addEventListener("resize", reset);
 }
 
 function respondToVisibility(element, callback, { thresholdRatio = 0 } = {}) {
@@ -151,6 +153,7 @@ function isInViewport(element, {outOfViewPortRatio = 1} = {}) {
         rect.bottom - (rect.height * outOfViewPortRatio) <= (window.innerHeight || document.documentElement.clientHeight)
     );
 }
+
 
 function updateCanvasSizes(canvas) {
 
@@ -236,6 +239,7 @@ function updateCanvasSizes(canvas) {
         ctx.fillRect(0, 0, canvas.width, canvas.height)
     }
 
+    let start, center, end, base;
     function init() {
         settings.startTime = new Date().getTime();
 
@@ -253,6 +257,35 @@ function updateCanvasSizes(canvas) {
                 nextImpactTime
             };
         });
+
+        const length = Math.min(canvas.width, canvas.height) * 0.9, offset = (canvas.width - length) / 2;
+
+        start = {
+            x: offset,
+            y: canvas.height / 2
+        };
+
+        end = {
+            x: canvas.width - offset,
+            y: canvas.height / 2
+        };
+
+        center = {
+            x: canvas.width / 2,
+            y: canvas.height / 2
+        };
+
+        const baseLength = end.x - start.x;
+        base = {
+            length: baseLength,
+            minAngle: 0,
+            startAngle: 0,
+            maxAngle: 2 * Math.PI,
+            initialRadius: baseLength * 0.05,
+            circleRadius: baseLength * 0.006,
+            clearance: baseLength * 0.03,
+        };
+        base.spacing = (base.length - base.initialRadius - base.clearance) / 2 / colors.length;
     }
 
     function drawArc(x, y, radius, start, end, fill = false) {
@@ -269,35 +302,6 @@ function updateCanvasSizes(canvas) {
 
         drawArc(position.x, position.y, pointRadius, 0, 2 * Math.PI, true);
     }
-
-    const length = Math.min(canvas.width, canvas.height) * 0.9, offset = (canvas.width - length) / 2;
-
-    const start = {
-        x: offset,
-        y: canvas.height / 2
-    };
-
-    const end = {
-        x: canvas.width - offset,
-        y: canvas.height / 2
-    };
-
-    const center = {
-        x: canvas.width / 2,
-        y: canvas.height / 2
-    };
-
-    const baseLength = end.x - start.x;
-    const base = {
-        length: baseLength,
-        minAngle: 0,
-        startAngle: 0,
-        maxAngle: 2 * Math.PI,
-        initialRadius: baseLength * 0.05,
-        circleRadius: baseLength * 0.006,
-        clearance: baseLength * 0.03,
-    };
-    base.spacing = (base.length - base.initialRadius - base.clearance) / 2 / colors.length;
 
     function nextFrame() {
         const currentTime = new Date().getTime();
@@ -346,7 +350,7 @@ function updateCanvasSizes(canvas) {
         ctx.fillText(secondsToHMS(settings.durationSeconds - elapsedTime % settings.durationSeconds), center.x, Math.floor(center.y + settings.fontSize / 2));
     }
 
-    startLoop(canvas, init, clear, nextFrame, { resetOnClick: true, resetOnScroll: true })
+    startLoop(canvas, init, clear, nextFrame, { resetOnClick: true, resetOnScroll: true, resetOnResize: true })
 }
 
 {
@@ -361,14 +365,14 @@ function updateCanvasSizes(canvas) {
     const trail = 20;
     const trailGap = 20;
 
-    const maxColum = canvas.width / fontSize;
+    let maxColum = canvas.width / fontSize;
 
     let fallingChars = [];
 
     class Characters {
         constructor(i = 1) {
             this.x = i * fontSize;
-            this.y = randomFloat(-500, 0);
+            this.y = randomFloat(-500, canvas.height / 10);
             this.dy = randomFloat(1, 5)
         }
 
@@ -390,6 +394,7 @@ function updateCanvasSizes(canvas) {
     }
 
     function init() {
+        maxColum = canvas.width / fontSize;
         fallingChars = Array.from({ length: maxColum }, (v, k) => new Characters(k))
     };
 
@@ -411,7 +416,7 @@ function updateCanvasSizes(canvas) {
         }
     }
 
-    startLoop(canvas, init, clear, nextFrame, { resetOnClick: true })
+    startLoop(canvas, init, clear, nextFrame, { resetOnClick: true, resetOnResize: true })
 }
 
 {
@@ -429,8 +434,8 @@ function updateCanvasSizes(canvas) {
 
     class Particle {
         constructor() {
-            this.x = randomFloat(0, canvas.width);
-            this.y = randomFloat(0, canvas.height);
+            this.x = randomFloat(0, 1);
+            this.y = randomFloat(0, 1);
 
             this.radius = randomFloat(1, 2);
             this.color = randomChoice(colors);
@@ -442,38 +447,57 @@ function updateCanvasSizes(canvas) {
         }
 
         draw() {
+
+            const [x, y] = [this.getX(), this.getY()]
+
             ctx.fillStyle = this.color;
             ctx.strokeStyle = this.color;
 
             ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius * this.connections.length, 0, Math.PI * 2, true);
+            ctx.arc(x, y, this.radius * this.connections.length, 0, Math.PI * 2, true);
             ctx.fill();
             ctx.closePath();
 
             ctx.beginPath();
-            ctx.arc(this.x, this.y, (this.radius + 5) * this.connections.length, 0, Math.PI * 2, true);
+            ctx.arc(x, y, (this.radius + 5) * this.connections.length, 0, Math.PI * 2, true);
             ctx.stroke();
             ctx.closePath();
 
             for (const otherParticle of this.connections) {
                 ctx.strokeStyle = this.color;
                 ctx.beginPath();
-                ctx.moveTo(this.x, this.y);
-                ctx.lineTo(otherParticle.x, otherParticle.y);
+                ctx.moveTo(x, y);
+                ctx.lineTo(otherParticle.getX(), otherParticle.getY());
                 ctx.stroke();
             }
         }
 
+        getX() {
+            return this.x * canvas.width;
+        }
+
+        getY() {
+            return this.y * canvas.height;
+        }
+
+        
+        findDistance(other) {
+            return Math.sqrt((this.getX() - other.getX()) ** 2 + (this.getY() - other.getY()) ** 2);
+        }
+
         update() {
-            this.x += this.vx;
-            this.y += this.vy;
+            this.x += (this.vx / canvas.width);
+            this.y += (this.vy / canvas.height);
 
-            if (this.x > canvas.width) this.x = 0;
-            if (this.x < 0) this.x = canvas.width;
-            if (this.y > canvas.height) this.y = 0;
-            if (this.y < 0) this.y = canvas.height;
+            if (this.x > 1) this.x = 0;
+            if (this.x < 0) this.x = 1;
+            if (this.y > 1) this.y = 0;
+            if (this.y < 0) this.y = 1;
 
-            this.connections = particles.filter((otherParticle) => this.color === otherParticle.color && findDistance(this, otherParticle) < distanceConnect);
+            this.connections = particles.filter(
+                (otherParticle) => this.color === otherParticle.color &&
+                this.findDistance(otherParticle) < distanceConnect
+            );
         }
     }
 
@@ -500,11 +524,7 @@ function updateCanvasSizes(canvas) {
         }
     }
 
-    function findDistance(p1, p2) {
-        return Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2);
-    }
-
-    startLoop(canvas, init, clear, nextFrame, { resetOnClick: true })
+    startLoop(canvas, init, clear, nextFrame, { resetOnClick: true, resetOnResize: false })
 }
 
 function makeGrid(width, height, func) {
@@ -519,8 +539,8 @@ function makeGrid(width, height, func) {
 
     const gridSize = 10;
 
-    const rows = Math.floor(canvas.height / gridSize);
-    const cols = Math.floor(canvas.width / gridSize);
+    let rows = Math.floor(canvas.height / gridSize);
+    let cols = Math.floor(canvas.width / gridSize);
 
     const startingChanceOn = 0.3;
 
@@ -554,8 +574,11 @@ function makeGrid(width, height, func) {
     const DEAD = 0;
 
     function init() {
+        rows = Math.floor(canvas.height / gridSize);
+        cols = Math.floor(canvas.width / gridSize);
         grid = makeGrid(rows, cols, () => Math.random() < startingChanceOn ? DEAD : randRGB());
         nextGrid = makeGrid(rows, cols, () => DEAD);
+        update();
     }
 
     function update() {
@@ -620,7 +643,7 @@ function makeGrid(width, height, func) {
         draw();
     }
 
-    startLoop(canvas, init, clear, nextFrame, { resetOnClick: true })
+    startLoop(canvas, init, clear, nextFrame, { resetOnClick: true, resetOnResize: true })
 }
 
 {
@@ -629,14 +652,7 @@ function makeGrid(width, height, func) {
 
     const ctx = canvas.getContext("2d");
 
-    const rows = 30;
-    const gridSize = canvas.height / rows;
-
-    const colsExtra = canvas.height % gridSize;
-    ctx.translate(colsExtra / 2, 0)
-
-    const cols = (canvas.width - colsExtra) / gridSize;
-
+    let rows, cols, gridSize;
 
     let grid;
     let currentTetromino;
@@ -645,11 +661,11 @@ function makeGrid(width, height, func) {
 
     class Tetromino {
         constructor(type, color) {
-            this.type = type;
+            this.shape = type;
             this.color = color;
 
             this.min_x = 0;
-            this.max_x = cols - this.type.length + 1;
+            this.max_x = cols - this.shape.length + 1;
 
             const pad = Math.floor(rows * 0.1);
             this.x = randomInt(pad, this.max_x - pad);
@@ -657,18 +673,27 @@ function makeGrid(width, height, func) {
         }
 
         new() {
-            return new Tetromino(this.type, this.color);
+            return new Tetromino(this.shape, this.color);
         }
 
         draw() {
-            for (let i = 0; i < this.type.length; i++) {
-                for (let j = 0; j < this.type[i].length; j++) {
-                    if (this.type[i][j]) {
+            for (let i = 0; i < this.shape.length; i++) {
+                for (let j = 0; j < this.shape[i].length; j++) {
+                    if (this.shape[i][j]) {
                         ctx.fillStyle = this.color;
                         ctx.fillRect((this.x + j) * gridSize, (this.y + i) * gridSize, gridSize, gridSize);
                     }
                 }
             }
+        }
+
+        getState() {
+            return [this.x, this.y, this.shape];
+        }
+
+        setState(state) {
+            // console.log(state);
+            [this.x, this.y, this.shape] = state;
         }
 
         moveDown() {
@@ -685,10 +710,10 @@ function makeGrid(width, height, func) {
 
         rotate() {
             const rotated = [];
-            for (let i = 0; i < this.type[0].length; i++) {
-                rotated.push(this.type.map(row => row[i]).reverse());
+            for (let i = 0; i < this.shape[0].length; i++) {
+                rotated.push(this.shape.map(row => row[i]).reverse());
             }
-            this.type = rotated;
+            this.shape = rotated;
         }
     }
 
@@ -716,6 +741,14 @@ function makeGrid(width, height, func) {
     ];
 
     function init() {
+        rows = 30;
+        gridSize = canvas.height / rows;
+
+        const colsExtra = canvas.height % gridSize;
+        ctx.translate(colsExtra / 2, 0)
+
+        cols = (canvas.width - colsExtra) / gridSize;
+
         grid = makeGrid(rows, cols, () => EMPTY);
         currentTetromino = null;
     }
@@ -726,46 +759,63 @@ function makeGrid(width, height, func) {
         if (currentTetromino === null) {
             currentTetromino = randomChoice(tetrominos).new();
 
-            const moveSet = randomChoice([
-                [currentTetromino.moveLeft, currentTetromino.moveLeft, currentTetromino.moveLeft, currentTetromino.moveLeft, currentTetromino.moveLeft, currentTetromino.moveLeft, currentTetromino.rotate],
-                [currentTetromino.moveRight, currentTetromino.moveRight, currentTetromino.moveRight, currentTetromino.moveRight, currentTetromino.moveRight, currentTetromino.moveRight, currentTetromino.rotate]
-            ]);
-
-            moveStack = Array.from({ length: randomInt(0, rows * 0.5) }, () => randomChoice(moveSet))
+            moveStack = getMoveStack(currentTetromino);
         }
 
-        if (canMoveDown(currentTetromino)) {
-            currentTetromino.moveDown();
-            const move = moveStack.pop();
-            if (move !== undefined) move.bind(currentTetromino)()
-        } else {
+        let oldState = currentTetromino.getState()
+        currentTetromino.moveDown();
+        if (doesCollide(currentTetromino)) {
+            currentTetromino.setState(oldState);
             placeTetrominoOnGrid(currentTetromino);
             currentTetromino = null;
         }
+        else {
+            oldState = currentTetromino.getState()
+            const move = moveStack.pop();
+            if (move !== undefined) move.bind(currentTetromino)()
+            if (doesCollide(currentTetromino)) {
+                currentTetromino.setState(oldState)
+            }
+        }
     }
 
-    function canMoveDown(tetromino) {
-        for (let i = 0; i < tetromino.type.length; i++) {
-            for (let j = 0; j < tetromino.type[i].length; j++) {
-                if (tetromino.type[i][j]) {
-                    const nextRow = tetromino.y + i + 1;
-                    if (nextRow >= rows || grid[nextRow][tetromino.x + j] !== EMPTY) {
-                        return false;
+    function doesCollide(tetromino) {
+        for (let i = 0; i < tetromino.shape.length; i++) {
+            for (let j = 0; j < tetromino.shape[i].length; j++) {
+                if (tetromino.shape[i][j]) {
+                    const [row, col] = [tetromino.y + i, tetromino.x + j];
+                    if (
+                        col < 0 ||
+                        row < 0 ||
+                        col >= cols ||
+                        row >= rows ||
+                        grid[row][col]
+                    ) {
+                        return true;
                     }
                 }
             }
         }
-        return true;
+        return false;
     }
 
     function placeTetrominoOnGrid(tetromino) {
-        for (let i = 0; i < tetromino.type.length; i++) {
-            for (let j = 0; j < tetromino.type[i].length; j++) {
-                if (tetromino.type[i][j]) {
+        for (let i = 0; i < tetromino.shape.length; i++) {
+            for (let j = 0; j < tetromino.shape[i].length; j++) {
+                if (tetromino.shape[i][j]) {
                     grid[tetromino.y + i][tetromino.x + j] = tetromino.color;
                 }
             }
         }
+    }
+
+    function getMoveStack(currentTetromino) {
+        // Update this to interact with grid and make a good ai
+        const moveSet = randomChoice([
+            [currentTetromino.moveLeft, currentTetromino.moveLeft, currentTetromino.moveLeft, currentTetromino.moveLeft, currentTetromino.moveLeft, currentTetromino.moveLeft, currentTetromino.rotate],
+            [currentTetromino.moveRight, currentTetromino.moveRight, currentTetromino.moveRight, currentTetromino.moveRight, currentTetromino.moveRight, currentTetromino.moveRight, currentTetromino.rotate]
+        ]);   
+        return Array.from({ length: randomInt(0, rows * 0.5) }, () => randomChoice(moveSet))
     }
 
     function clear() {
@@ -793,5 +843,5 @@ function makeGrid(width, height, func) {
         draw();
     }
 
-    startLoop(canvas, init, clear, nextFrame, { resetOnClick: true })
+    startLoop(canvas, init, clear, nextFrame, { resetOnClick: true, resetOnResize: true })
 }
