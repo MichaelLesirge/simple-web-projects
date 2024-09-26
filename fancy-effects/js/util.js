596e495ad699c381e50adf5a3b4a4faec60bb426
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2024-09-25 19:45:37 -0700

    add new util functions for shuffling

--- fancy-effects/js/util.js
+++ fancy-effects/js/util.js
@@ -15,6 +15,28 @@ export function randomInt(min, max) {
     return Math.floor(randomFloat(min, max))
 }
 
+export function shuffle(array) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+}
+
+export function shuffledCopy(array) {
+    array = array.slice();
+    shuffle(array);
+    return array;
+}
+
 export function makeGrid(width, height, func) {
     return Array.from({ length: width }, () => Array.from({ length: height }, func));;
 }
