c91261007559343cb30dde5df285de89c58b7da8
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2024-10-19 16:32:41 -0700

    update label locations and file upload

--- xbox-diagram-maker/script.js
+++ xbox-diagram-maker/script.js
@@ -1,31 +1,32 @@
 const canvas = document.getElementById("controllerCanvas");
 const ctx = canvas.getContext("2d");
 
-const LEFT = 159;
-const RIGHT = 485;
-const rows = [21, 60, 94, 292, 318, 345, 376];
+const DEBUG = false;
+
+const LEFT = 157+2;
+const RIGHT = 485-2;
 
 const buttonPositions = {
-    "View": [LEFT, rows[0]],
-    "Menu": [RIGHT, rows[0]],
-    "Left Bumper": [LEFT, rows[1]],
-    "Right Bumper": [RIGHT, rows[1]],
-    "Left Trigger": [LEFT, rows[2]],
-    "Right Trigger": [RIGHT, rows[2]-8],
-    "Left Stick Y Axis": [LEFT, 130],
-    "Left Stick X Axis": [LEFT, 160],
-    "Left Stick Button": [LEFT, 190],
-    "Y": [RIGHT, 118],
-    "B": [RIGHT, 161],
-    "X": [RIGHT, 183],
+    "View": [LEFT, 17],
+    "Menu": [RIGHT, 17],
+    "Left Bumper": [LEFT, 55],
+    "Right Bumper": [RIGHT, 53],
+    "Left Trigger": [LEFT, 89],
+    "Right Trigger": [RIGHT, 80],
+    "Left Stick Y Axis": [LEFT, 128],
+    "Left Stick X Axis": [LEFT, 156],
+    "Left Stick Button": [LEFT, 186],
+    "Y": [RIGHT, 114],
+    "B": [RIGHT, 160],
+    "X": [RIGHT, 182],
     "A": [RIGHT, 273],
-    "D-pad Up": [LEFT, rows[3]],
-    "D-pad Left": [LEFT, rows[4]],
-    "D-pad Down": [LEFT, rows[5]],
-    "D-pad Right": [LEFT, rows[6]],
-    "Right Stick Y Axis": [RIGHT, rows[4]],
-    "Right Stick X Axis": [RIGHT, rows[5]],
-    "Right Stick Button": [RIGHT, rows[6]],
+    "D-pad Up": [LEFT, 292],
+    "D-pad Left": [LEFT, 320],
+    "D-pad Down": [LEFT, 347],
+    "D-pad Right": [LEFT, 378],
+    "Right Stick Y Axis": [RIGHT, 320],
+    "Right Stick X Axis": [RIGHT, 347],
+    "Right Stick Button": [RIGHT, 376],
 };
 
 let title = "";
@@ -43,13 +44,20 @@ function drawController() {
     ctx.font = "32px Arial";
     ctx.fillText(title, 5, 18);
 
+    for (const [button, position] of Object.entries(DEBUG ? buttonPositions : {})) {
+        ctx.fillStyle = "red";
+        ctx.font = "16px Arial";
+        ctx.direction = position[0] === LEFT ? "rtl" : "ltr";
+        ctx.fillText(`-------{${button}}------`, position[0], position[1] + 16/3);
+    }
+
     for (const [button, label] of Object.entries(labels)) {
         const position = buttonPositions[button];
         if (position && label) {
             ctx.fillStyle = "#000";
             ctx.font = "16px Arial";
             ctx.direction = position[0] === LEFT ? "rtl" : "ltr";
-            ctx.fillText(label, position[0], position[1] + 16/3);
+            ctx.fillText(label, position[0], position[1] + 16/2.8);
         }
     }
 
