dddd48d7609a37303d0a9b7961fa4875f93d2cfd
MichaelLesirge <michael.lesirge@gmail.com>
2025-07-07 20:26:36 -0700

    fix formatting of one second game

--- one-second/script.js
+++ one-second/script.js
@@ -7,40 +7,37 @@ const resetButton = document.getElementById("reset");
 const scoreDisplay = document.getElementById("score");
 
 const settings = {
-    winMilliseconds: 1000,
-    advStats: false,
-    oneTouchMode: true,
-}
+	winMilliseconds: 1000,
+	advStats: false,
+	oneTouchMode: true,
+};
 
 const urlParams = new URLSearchParams(window.location.search);
 
 if (urlParams.has("ms")) {
-    settings.winMilliseconds = parseInt(urlParams.get("ms"));
+	settings.winMilliseconds = parseInt(urlParams.get("ms"));
 }
 if (urlParams.has("s")) {
-    settings.advStats = urlParams.get("s") === "true";
+	settings.advStats = urlParams.get("s") === "true";
 }
 if (urlParams.has("t")) {
-    settings.oneTouchMode = urlParams.get("t") === "true";
+	settings.oneTouchMode = urlParams.get("t") === "true";
 }
 
 console.table(settings);
-
 
 function millisToText(milliseconds) {
 	const centiseconds = Math.floor(milliseconds / 10) % 100;
 	const seconds = Math.floor(milliseconds / 1000) % 60;
 	const minutes = Math.floor(milliseconds / 60000);
 
-	return`${minutes.toString().padStart(2, "0")}:${seconds
-		.toString()
-		.padStart(2, "0")}.${centiseconds.toString().padStart(2, "0")}`;
+	return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}.${centiseconds.toString().padStart(2, "0")}`;
 }
 
 function update() {
 	const milliseconds = (runStartTime === null ? 0 : Date.now() - runStartTime) + pastTime;
 
-    timerDisplay.textContent = millisToText(milliseconds)
+	timerDisplay.textContent = millisToText(milliseconds);
 
 	requestAnimationFrame(update);
 }
@@ -72,28 +69,28 @@ function toggleStart() {
 		startButton.classList.remove("stop");
 
 		update();
-        checkWin();
+		checkWin();
 	}
 }
 
 function checkWin() {
 	if (timerDisplay.textContent === millisToText(settings.winMilliseconds)) {
-        addWin();
-        updateWinsDisplay();
+		addWin();
+		updateWinsDisplay();
 		createConfetti();
 	}
 }
 
 const winsTotalKey = "winsTotal";
-function addWin() {   
-    localStorage.setItem(winsTotalKey, (parseInt(localStorage.getItem(winsTotalKey)) || 0) + 1);
+function addWin() {
+	localStorage.setItem(winsTotalKey, (parseInt(localStorage.getItem(winsTotalKey)) || 0) + 1);
 }
 
 function updateWinsDisplay() {
-    if (settings.advStats) {
-    } else {
-        scoreDisplay.textContent = `Wins: ${localStorage.getItem(winsTotalKey) || 0}`;
-    }
+	if (settings.advStats) {
+	} else {
+		scoreDisplay.textContent = `Wins: ${localStorage.getItem(winsTotalKey) || 0}`;
+	}
 }
 
 updateWinsDisplay();
@@ -110,32 +107,32 @@ startButton.addEventListener("click", toggleStart);
 resetButton.addEventListener("click", reset);
 
 if (settings.oneTouchMode) {
-    const tapAction = (event) => {
-        if (event.target === startButton || event.target === resetButton) {
-            return;
-        }
-        if (timerDisplay.textContent === millisToText(0)) {
-            toggleStart();
-        } else if (runStartTime !== null) {
-            toggleStart();
-        } else {
-            reset();
-        }
-        event.preventDefault();
-    }
-    // document.addEventListener("touchstart", tapAction);
-    document.addEventListener("mousedown", tapAction);
+	const tapAction = (event) => {
+		if (event.target === startButton || event.target === resetButton) {
+			return;
+		}
+		if (timerDisplay.textContent === millisToText(0)) {
+			toggleStart();
+		} else if (runStartTime !== null) {
+			toggleStart();
+		} else {
+			reset();
+		}
+		event.preventDefault();
+	};
+	// document.addEventListener("touchstart", tapAction);
+	document.addEventListener("mousedown", tapAction);
 }
 
 document.addEventListener("keydown", (event) => {
-    if (event.key === " ") {
-        event.preventDefault();
-        toggleStart();
-    }
-    if (event.key === "r" || event.key === "enter") {
-        event.preventDefault();
-        reset();
-    }
+	if (event.key === " ") {
+		event.preventDefault();
+		toggleStart();
+	}
+	if (event.key === "r" || event.key === "enter") {
+		event.preventDefault();
+		reset();
+	}
 });
 
 update();
